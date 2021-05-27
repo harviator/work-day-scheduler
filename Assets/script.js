eb773463fba27aceb45206695cd5e94f5fa27e95
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-27 17:16:28 -0400

    made notes in js file to stay focused and on track

--- Assets/script.js
+++ Assets/script.js
@@ -4,3 +4,15 @@ function displayTime(){
 };
 
 setInterval(displayTime,1000);
+
+/*
+Notes:
+1. Need to show the date
+2. Time blocks for 9-5
+3. Evaluate past, present, and future
+4. Colour code past, present, and future
+5. Enter text in time block
+6. Save button
+7. Save button saves text to local storage
+8. Saved text stays with each refresh
+*/
