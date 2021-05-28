0e29a7af8fb069a16a1410d78a07e7e978790155
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-27 21:06:30 -0400

    added if statment for colour coding, but need a loop

--- Assets/script.js
+++ Assets/script.js
@@ -32,9 +32,8 @@ pastPresentFuture();
 Notes:
 1. Need to show the date - Done
 2. Time blocks for 9-5 - Done
-3. Evaluate past, present, and future
-
-4. Colour code past, present, and future
+3. Evaluate past, present, and future - Done
+4. Colour code past, present, and future - Done
 5. Enter text in time block
 6. Save button
 7. Save button saves text to local storage
