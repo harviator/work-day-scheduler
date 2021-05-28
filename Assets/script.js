8fc88475ecb79e550fc9b3ae9ef1fe13b490c5f2
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-27 20:47:35 -0400

    added if statment for colour coding, but need a loop

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,5 @@
+//Global Variables
+
 function displayTime(){
     var dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(dateTime);
@@ -5,20 +7,32 @@ function displayTime(){
 
 setInterval(displayTime,1000);
 
-// var nineToFive = [
-//     {
-//         block: 0,
-//         hour: 
 
-//     }
-// ]
+function pastPresentFuture() {
 
+    var time = 12; //moment().format('H');
+    console.log(typeof time);
+
+    var block =  $('.row').attr('id');
+    console.log(block);
+
+    if (block == time) {
+        $('row').addClass('present');
+    } else if (block < time) {
+        $('row').addClass('past');
+    } else {
+        $('row').addClass('future');
+    }
+};
+
+pastPresentFuture();
 
 /*
 Notes:
 1. Need to show the date - Done
-2. Time blocks for 9-5
+2. Time blocks for 9-5 - Done
 3. Evaluate past, present, and future
+
 4. Colour code past, present, and future
 5. Enter text in time block
 6. Save button
