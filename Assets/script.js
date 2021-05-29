9a9c7c3a1617c0a181f387ec7ad330281c75cfbd
Harviator <78506767+harviator@users.noreply.github.com>
2021-05-28 21:05:37 -0400

    website is functioning properly.  added some additional styling

--- Assets/script.js
+++ Assets/script.js
@@ -19,11 +19,11 @@ function pastPresentFuture() {
         var block =  $(this).attr('id');
     
         if (block == time) {
-            $(this).addClass('present');
+            $(this).children().eq(1).addClass('present');
         } else if (block < time) {
-            $(this).addClass('past');
+            $(this).children().eq(1).addClass('past');
         } else {
-            $(this).addClass('future');
+            $(this).children().eq(1).addClass('future');
         }
     })
 };
@@ -34,43 +34,27 @@ pastPresentFuture();
 function saveText() {
 
     var hour = $(this).parents().attr('id');
-    
     var sched = $(this).siblings().eq(1).val();
+    console.log(hour);
+    console.log(sched);
     
     localStorage.setItem(hour, sched);
 }
 
-function Load(event) {
-
-    //var hour = $(this);
-    
-    //var sched = $(this).siblings().eq(1).val()
+function Load() {
 
     $('#09').children().eq(1).val(localStorage.getItem('09'));
-
-    console.log(hour);
-    console.log(sched);
-  
-    //#09 - is the id... (localStorage.getItem('09')) 09 - is the key...
+    $('#10').children().eq(1).val(localStorage.getItem('10'));
+    $('#11').children().eq(1).val(localStorage.getItem('11'));
+    $('#12').children().eq(1).val(localStorage.getItem('12'));
+    $('#13').children().eq(1).val(localStorage.getItem('13'));
+    $('#14').children().eq(1).val(localStorage.getItem('14'));
+    $('#15').children().eq(1).val(localStorage.getItem('15'));
+    $('#16').children().eq(1).val(localStorage.getItem('16'));
+    $('#17').children().eq(1).val(localStorage.getItem('17'));
     
 }
 
 Load()
 saveBtn.on('click', saveText);
 
-/*
-Notes:
-1. Need to show the date - Done
-2. Time blocks for 9-5 - Done
-3. Evaluate past, present, and future - Done
-4. Colour code past, present, and future - Done
-5. Enter text in time block - Done
-6. Save button - Done
-7. Save button saves text to local storage
-8. Saved text stays with each refresh
-
-Issues:
-1. Green background colors extents behind save button
-2. 9am is green all times (from yesterday evening to this morning.  This morning all the blocks should be green but they aren't)
-
-*/
